a711cb5a304599f45e9a2752663a77d9cabcf369
testName <test>
2024-04-26 18:45:00 +0300

    Урок 2

--- LessonTasks/Program.cs
+++ LessonTasks/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LessonTasks
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonTasks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello, World!");
+            Console.WriteLine("БВ321 привет! Урок 2");
         }
     }
 }
