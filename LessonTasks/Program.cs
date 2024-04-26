d8925c61665fd30c65b1af24bc88386b2f09f1e9
testName <test>
2024-04-26 22:02:48 +0300

    Незаконченная практика

--- LessonTasks/Program.cs
+++ LessonTasks/Program.cs
@@ -15,7 +15,7 @@ namespace LessonTasks
             switch (taskNumber)
             {
                 case 1: SolveTask1(); break;
-                case 2: SolveTask2(); break;
+                case 2: SolveTask2(args); break;
                 case 3: SolveTask3(); break;
                 case 4: SolveTask4(); break;
                 default: Console.WriteLine("Unknown task"); break;
@@ -24,12 +24,48 @@ namespace LessonTasks
 
         private static void SolveTask1()
         {
-            
+            var arr = new int[5];
+            int evenCount = 0;
+            int oddCount = 0;
+            var hashSet = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int item = arr[i];
+                if (item % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+
+                hashSet.Add(item);
+            }
+
+            Console.WriteLine(String.Format("{0} {1}", evenCount, oddCount));
+            Console.WriteLine($"{evenCount} {oddCount} {hashSet.Count}");
+            Console.ReadKey();
         }
 
-        private static void SolveTask2()
+        private static void SolveTask2(string[] args)
         {
-            
+            int result = 0;
+
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(',');
+            int upperBoundary = ParseSingleNumberArgument(args);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int num = int.Parse(numbers[i]);
+                if (num < upperBoundary)
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine($"Thank you for your attention: {result}");
+            Console.ReadKey();
         }
 
         private static void SolveTask3()
@@ -39,7 +75,15 @@ namespace LessonTasks
 
         private static void SolveTask4()
         {
-            
+
+        }
+
+        private static int ParseSingleNumberArgument(string[] args)
+        {
+            if (args.Length != 1)
+                throw new ApplicationException("args.Length != 1");
+
+            return int.Parse(args[0]);
         }
     }
 }
