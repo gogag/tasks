4ecf906bf2eadf00f1c6556dc51d4fc487ee225d
testName <test>
2024-05-27 22:02:51 +0300

    Урок N+1. Совместная практика по примеру с генерацией
    - Раскрыли подробнее именование, переход от процедурного программирования (static) к ООП, структурирование решения сборками, пространства имен.
    - Повторили обработку исключений.
    - Сгенерировали класс и его метод (публичный контракт), без написания конкретного кода, и смогли закончить написание логики взаимодействия с пользователем.
    - Написали реализацию класса для генерации нечетных чисел.
    - Добавили в решение отдельную сборку LessonTasks.NumberGenerators (библиотеку .NET Framework), чтобы отделить "ядро" для решаемой задачи от логики взаимодействия с пользователем, сохраняя при этом структуру пространств имен, таким же, как было бы просто с вложенной папкой. Имя NumberGenerators уже фиксирует некую абстракцию, хотя физически мы еще не выделили соответсвующих контрактов - на следующем занятии.

---
+++ LessonTasks.NumberGenerators/OddNumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace LessonTasks.NumberGenerators
+{
+    public class OddNumberGenerator
+    {
+        private int _current;
+
+        public OddNumberGenerator()
+        {
+            _current = 1;
+        }
+
+        public int Next()
+        {
+            int current = _current;
+            _current = current + 2;
+            return current;
+        }
+    }
+}
--- LessonTasks/Program.cs
+++ LessonTasks/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LessonTasks.NumberGenerators;
 
 namespace LessonTasks
 {
     internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Enter task number:");
             int taskNumber = int.Parse(Console.ReadLine());
@@ -20,11 +21,49 @@ namespace LessonTasks
                 case 4: SolveTask4(); break;
                 default: Console.WriteLine("Unknown task"); break;
             }
+
+            Console.ReadKey();
         }
 
         private static void SolveTask1()
         {
-            
+            try
+            {
+                SolveTask1Handled();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ooops.. Something went wrong:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void SolveTask1Handled()
+        {
+            Console.WriteLine("Enter count of numbers to generate:");
+            int countToGenerate = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose one of supported number generators: odd");
+            string generatorName = Console.ReadLine();
+
+            OddNumberGenerator numberGenerator;
+
+            switch (generatorName)
+            {
+                case "odd":
+                    numberGenerator = new OddNumberGenerator();
+                    break;
+                default:
+                    throw new ApplicationException($"Unknown generator: {generatorName}");
+            }
+
+            for (int i = 0; i < countToGenerate; i++)
+            {
+                int generatedNumber = numberGenerator.Next();
+                Console.WriteLine($"Generated number {i + 1}: {generatedNumber}");
+            }
+
+            Console.WriteLine("Press any key to finish...");
         }
 
         private static void SolveTask2()
