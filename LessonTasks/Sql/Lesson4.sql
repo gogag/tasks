49399c58eeb6457643f1702ac69aa6cc17fc8461
Georgii Kalashnikov <gzorik.us@gmail.com>
2024-06-02 01:58:07 +0300

    Урок 4. Добавил ссылок и примеров проблем с NULL

--- LessonTasks/Sql/Lesson4.sql
+++ LessonTasks/Sql/Lesson4.sql
@@ -6,13 +6,33 @@ end as Col1
 
 create table TableWithNull (
 	Col int null
+,	Col2 int null
 )
 
 select * from TableWithNull where Col IS NOT NULL AND Col > 0
 insert into TableWithNull values (null)
+insert into TableWithNull values (100500)
+insert into TableWithNull (Col2) values (22)
+
+-- тут про особое отношение AND и OR к NULL операндам, что позволяет комбинировать IS NULL проверки с обычными предикатами над null:
+-- https://learn.microsoft.com/ru-ru/sql/t-sql/language-elements/null-and-unknown-transact-sql?view=sql-server-ver16
 
 select * from TableWithNull where Col = null -- не работает. результат - null
+select * from TableWithNull where Col != null -- не работает. результат - null
+select * from TableWithNull where Col != 2 -- упс, запись с null тоже не подтянется в результат, тогда как в шарпе null != 2 это истина
+select * from TableWithNull where not (Col = 2) -- не поможет, запись с null все равно не подтянется в результат
+select * from TableWithNull where Col not in (2) -- упс, запись с null тоже не подтянется в результат, тогда как в шарпе null != 2 это истина
+select Col, count(*) from TableWithNull group by Col -- а вот значением группы null может оставаться
+declare @someVar varchar;
+select 'asdf' + @someVar + 'asdf' -- null. лечится CONCAT-ом
+select concat('asdf', @someVar, 'asdf') -- asdfasdf
+select count(*) from TableWithNull -- 2
+select count(Col) from TableWithNull -- 1 -- потому что null значения исключаются из агреггирующей функции
 select * from TableWithNull where Col IS NULL -- только так
+select * from TableWithNull where Col IS NOT NULL -- только так
+select * from TableWithNull where Col IS NULL OR Col != 2 -- OR решает проблему
+select * from TableWithNull where Col = 22 OR Col2 = 22 -- и не только с Col IS NULL, но в таком варианте могут быть проблемы с оптимизацией запросов
+select * from TableWithNull where ISNULL(Col, Col2) = 22 -- еще как альтернатива записи, но в оптимизации не поможет
 
 --bool ? trueVal : falseValue
 --reference ?? throw new Exc
